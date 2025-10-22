535012ce80dbe4114439170a3563600c249300c1
Isaac <isaaconkeys@protonmail.com>
2025-10-22 18:04:11 +0200

    Fix base for GH Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://isaaconkeys.github.io",
-  base: "/Cocoa_Prices/",
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  base: "/devportfolio/",
+  vite: { plugins: [tailwindcss()] },
 });
