a60f54448c12b78bb8a4129ce3958ae5ffcafc06
Isaac <isaaconkeys@protonmail.com>
2025-10-22 17:44:19 +0200

    prepare for production

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://isaaconkeys.github.io",
+  base: "/Cocoa_Prices/",
   vite: {
     plugins: [tailwindcss()],
   },
